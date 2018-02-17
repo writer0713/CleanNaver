7b4d49042b9dc1a3ce0cf9f67f2b287d3d67e385
kimjunghwan <writer0713@naver.com>
2018-02-17 20:32:45 +0900

    initial commit

---
+++ content.js
@@ -0,0 +1,33 @@
+console.log("content.js");
+
+(function($) {
+    
+    this.commentBox = $('#cbox_module');
+    this.toggleButton = $(`
+    <ul class="tg-list">
+        <li class="tg-list-item" id="nc_toggle_btn">
+            <h4>댓글 컨트롤</h4>
+            <input class="tgl tgl-flip" id="cb5" type="checkbox"/>
+            <label class="tgl-btn" data-tg-off="보이기" data-tg-on="가리기" for="cb5"></label>
+        </li>
+    </ul>
+    `);
+
+    this.commentBox.before(this.toggleButton);
+    this.commentBox.hide();
+    
+    $('body').scrollTop();
+
+
+    $('#nc_toggle_btn').on('click', () => {
+        let checkbox = $('#nc_toggle_btn input');
+        let isChecked = checkbox.prop('checked');
+
+        if(isChecked) {
+            this.commentBox.show();
+        } else {
+            this.commentBox.hide();
+        }
+    });
+
+})(jQuery);
