8d14cd3c464c9a2634784b1a3decb74205d72e87
kimjunghwan <writer0713@naver.com>
2018-02-17 23:14:14 +0900

    연예, 스포츠 뉴스에서 css가 이상하게 적용되어 css 변경 - 버튼 영역에 테두리 추가 - margin, padding 속성 추가 (가운데 정렬) - 네이버에서 제공하는 '댓글 접기 관련 영역' 있을시 안보이도록 코드 추가

--- content.js
+++ content.js
@@ -1,20 +1,23 @@
-console.log("content.js");
-
 (function($) {
     
     this.commentBox = $('#cbox_module');
-    this.toggleButton = $(`
-    <ul class="tg-list">
-        <li class="tg-list-item" id="nc_toggle_btn">
-            <h4>댓글 컨트롤</h4>
-            <input class="tgl tgl-flip" id="cb5" type="checkbox"/>
-            <label class="tgl-btn" data-tg-off="보이기" data-tg-on="가리기" for="cb5"></label>
-        </li>
-    </ul>
+    this.commentGuideDiv = $('.comment_guide');
+    this.toggleButtonDiv = $(`
+    <div class="tg-div">
+        <ul class="tg-list">
+            <li class="tg-list-item" id="nc_toggle_btn">
+                <h4>댓글 컨트롤</h4>
+                <input class="tgl tgl-flip" id="cb5" type="checkbox"/>
+                <label class="tgl-btn" data-tg-off="보이기" data-tg-on="가리기" for="cb5"></label>
+            </li>
+        </ul>
+    </div>
     `);
 
-    this.commentBox.before(this.toggleButton);
+    this.commentBox.before(this.toggleButtonDiv);
     this.commentBox.hide();
+
+    this.commentGuideDiv.remove();
     
     $('body').scrollTop();
 
