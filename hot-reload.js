6beb6ea51c9d52ffb51c0337aaffa0f90d9ac03e
kimjunghwan <writer0713@naver.com>
2018-05-09 13:46:56 +0900

    크롬 익스텐션 hot-reload 기능 추가

---
+++ hot-reload.js
@@ -0,0 +1,61 @@
+const filesInDirectory = dir => new Promise (resolve =>
+
+    dir.createReader().readEntries (entries =>
+
+        Promise.all (entries.filter (e => {
+            //return e.name[0] !== '.';
+            //console.log(e);
+            return e.name === 'dist' || e.name === 'bundle.js';
+        }).map (e =>
+
+            e.isDirectory
+                ? filesInDirectory (e)
+                : new Promise (resolve => e.file (resolve))
+        ))
+        .then (files => [].concat (...files))
+        .then (resolve)
+    )
+);
+
+const timestampForFilesInDirectory = dir =>
+        filesInDirectory (dir).then (files => {
+            return files.map (f => f.name + f.lastModifiedDate).join();
+        });
+
+const reload = () => {
+
+    chrome.tabs.query ({ active: true, currentWindow: true }, tabs => {
+
+        if (tabs[0]) { 
+            chrome.tabs.reload (tabs[0].id) 
+        }
+
+        chrome.runtime.reload ()
+    });
+};
+
+const watchChanges = (dir, lastTimestamp) => {
+
+    timestampForFilesInDirectory (dir).then (timestamp => {
+
+        if (!lastTimestamp || (lastTimestamp === timestamp)) {
+            console.log('checking');
+            setTimeout (() => watchChanges (dir, timestamp), 1000); // retry after 1s
+
+        } else {
+            console.log('reload');
+            reload ();
+        }
+    });
+
+};
+
+chrome.management.getSelf (self => {
+
+    if (self.installType === 'development') {
+
+        chrome.runtime.getPackageDirectoryEntry (dir => {
+            watchChanges (dir)
+        });
+    }
+});
